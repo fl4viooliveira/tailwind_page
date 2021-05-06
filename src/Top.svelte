a3bef9d6a657dea5099d515946063544526c3e14
fl4viooliveira <fl4viooliveira@gmail.com>
2021-05-07 00:05:50 +0100

    Increase the avatar image size
    Added the close button on mobile menu

--- src/Top.svelte
+++ src/Top.svelte
@@ -15,32 +15,51 @@
       </svg>
       <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase mr-15">Company</span>
     </a>
-    <img class="h-8 w-8 rounded-full object-center" src="https://avatars.githubusercontent.com/u/101659?v=4" alt="Mat">
+    <img class="h-12 w-12 rounded-full object-center mx-5" src="https://avatars.githubusercontent.com/u/101659?v=4" alt="Mat">
     <!-- Mobile menu -->
     <div class:active on:click={() => active= !active} class="lg:hidden">
-      <button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50">
+      <button id="open" class="p-2 -mr-1 transition duration-200 rounded 
+        focus:outline-none focus:shadow-outline hover:bg-purple-50 focus:bg-deep-purple-50">
         <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
           <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
           <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
           <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
         </svg>
       </button>
+      <button id="close" class="p-2 -mr-1 transition duration-200 rounded 
+        hover:bg-purple-50 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+        <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+          <path fill="currentColor"
+            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 
+            c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 
+            c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z">
+          </path>
+        </svg>
+      </button>
     </div>
   </div>
 </div>
-
-  <div class="side-bar items-stretch md:items-center" class:active>
-    <Sidebar />
-  </div>
+<div class="side-bar items-stretch md:items-center" class:active>
+  <Sidebar />
+</div>
 
 <style>
   .side-bar{
-    display:none;
+    display: none;
+  }
+  #close{
+    display: none;
   }
   .active{
     display: inline-table;
     width: 100%;
     text-align: right;    
   }
-
+  .active #open{
+    display: none;
+  }
+  .active #close{
+    display: inline-table;
+    text-align: right;
+  }
 </style>
